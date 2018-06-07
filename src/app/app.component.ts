1e0d0cec9d4b22af6fed2209a80f2a649f751012
Giorgio Mazzei <giorgio.mazzei@outlook.com>
2018-06-08 00:17:30 +0200

    UI structure done - click method for touchpad buttons added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  private dialledNumber: string;
+  ngOnInit(): void {
+    this.dialledNumber = 'dsfs';
+  }
+
+  private touchPadButtonClicked(event) {
+    console.log('event: ', event.path[0].innerText);
+  }
+
 }
